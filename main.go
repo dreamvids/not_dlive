35d189f2c80a068352e91f1400956893ecb33c6f
quadrifoglio <clement@dreamvids.fr>
2015-12-16 11:39:14 +0100

    Fresh start

--- main.go
+++ main.go
@@ -1,101 +1,41 @@
 package main
 
 import (
-	//"database/sql"
-	"encoding/json"
-	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/mux"
-
-	//"github.com/dreamvids/dlive/chat"
+	"github.com/dreamvids/dlive/chat"
 	"github.com/dreamvids/dlive/stream"
+	"github.com/gorilla/mux"
 )
 
-const (
-	Name    string = "DreamVids Live Server"
-	Version string = "1.0"
-)
+func handlePushStream(w http.ResponseWriter, r *http.Request) {
+	v := mux.Vars(r)
+	log.Println("Push", v["id"])
 
-type serverConfig struct {
-	HttpPort int `json:"http-port"`
-
-	MaxClients  int    `json:"chat-max-clients"`
-	ModoRank    int    `json:"chat-modo-rank"`
-	AdminRank   int    `json:"chat-admin-rank"`
-	MuteMessage string `json:"chat-mute-message"`
-
-	DbHost string `json:"db-host"`
-	DbUser string `json:"db-username"`
-	DbPass string `json:"db-password"`
-	DbName string `json:"db-name"`
+	stream.Push(v["id"], w, r)
 }
 
-func parseConfig(path string) (serverConfig, error) {
-	var config serverConfig
+func handlePullStream(w http.ResponseWriter, r *http.Request) {
+	v := mux.Vars(r)
+	log.Println("Pull", v["id"])
 
-	file, err := ioutil.ReadFile(path)
-	if err != nil {
-		return config, err
-	}
-
-	err = json.Unmarshal(file, &config)
-	if err != nil {
-		return config, err
-	}
-
-	return config, nil
+	stream.Push(v["id"], w, r)
 }
 
 func main() {
-	configPath := flag.String("config", "dlive.json", "Configuration file to use")
-	logPath := flag.String("log", "", "Log file to use")
-	flag.Parse()
-
-	if len(*logPath) > 0 {
-		f, err := os.OpenFile(*logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			log.Println("Can not open log file:", err)
-		} else {
-			log.SetOutput(f)
-		}
-		defer f.Close()
-	}
-
-	c, err := parseConfig(*configPath)
-	if err != nil {
-		log.Fatalf("Fatal error while parsing config: %s", err)
-	}
-
-	/*log.Println("Connecting to database...")
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", c.DbUser, c.DbPass, c.DbHost, c.DbName))
-	if err != nil {
-		log.Fatalf("Database error: %s", err)
-	}
-	defer db.Close()
-
-	if err = db.Ping(); err != nil {
-		log.Fatalf("Database error: %s", err)
-	}
-	log.Println("Connected to database !")
-
-	chat.Init(db)*/
+	log.Println("dlive server")
 
 	r := mux.NewRouter()
-	//r.HandleFunc("/", chat.HandleWebsocket)
 
-	r.HandleFunc("/stream/push/{id}", stream.HandlePush).Methods("POST")
-	r.HandleFunc("/stream/pull/{id}", stream.HandlePull).Methods("GET")
-
-	r.HandleFunc("/stream/player", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "stream_player.html")
-	})
-
+	r.HandleFunc("/stream/push/{id}", handlePushStream)
+	r.HandleFunc("/stream/pull/{id}", handlePullStream)
 	http.Handle("/", r)
-	log.Println("Listening on port", c.HttpPort)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.HttpPort), nil))
+
+	err := chat.BindServer("/chat")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- stream.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ffmpeg -f video4linux2 -s 640x480 -r 25 -i /dev/video0 -threads 3 -f webm http://127.0.0.1:8081/stream/push/mdr
-#ffmpeg -f video4linux2 -s 640x480 -r 25 -i /dev/video0 -f webm video.webm
